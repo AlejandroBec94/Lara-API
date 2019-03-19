66c645a4c61d538b143bdcfd393b9ebff422d850
Bec <a.becor94@gmail.com>
2019-03-19 11:24:31 -0600

    Format json api

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -10,18 +10,24 @@ class ProductResource extends Resource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
 //        return parent::toArray($request);
         return [
-            'name'=>$this->name,
-            'detail'=>$this->detail,
-            'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount,
+            'name' => $this->name,
+            'detail' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price),
+//            'rating' => round($this->reviews->sum('star')/$this->reviews->count(),2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet.',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
